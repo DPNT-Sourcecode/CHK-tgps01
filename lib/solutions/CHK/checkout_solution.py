0aec1be8d18afa907ae4f2842ee805146166f0bf
sbx_user1051 <sbx_user1051@169.254.13.165>
2022-05-06 20:20:20 +0000

    Fri May 06 20:20:20 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,12 +3,21 @@
 # skus = unicode string
 def checkout(skus):
     try:
-        return checkout_implementation(skus)
+        return checkout_impl(skus)
     except Exception:
         return -1
 
 
-def checkout_implementation(input):
+PRICES = {
+    "A": 50,
+    "B": 30,
+    "C": 20,
+    "D": 15
+}
+
+
+def checkout_impl(input):
     """Implementation of the function which raises exceptions instead of returning -1"""
     pass
+
 
---
+++ test/solution_tests/CHK/test_part1.py
@@ -0,0 +1,4 @@
+from solutions.CHK.checkout_solution import checkout_impl
+
+def test_a():
+
