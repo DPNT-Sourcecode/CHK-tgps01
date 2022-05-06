25532b8881b754b3fd7d7ea93e092e8859056aaf
sbx_user1051 <sbx_user1051@169.254.13.165>
2022-05-06 22:39:19 +0000

    Fri May 06 22:39:19 UTC 2022

--- test/solution_tests/CHK/test_part5.py
+++ test/solution_tests/CHK/test_part5.py
@@ -1,16 +1,26 @@
 from solutions.CHK.checkout_solution import apply_combibuy
 
+import pytest
 
-def test_one():
-    counts = {
-        "Z": 5,
-        "Y": 2
+
+@pytest.fixture
+def item_counts():
+    return {
+        "S": 0,
+        "T": 0,
+        "X": 0,
+        "Y": 0,
+        "Z": 0
     }
-    discount = apply_combibuy(counts)
+
+
+def test_one(item_counts):
+    item_counts["Z"] = 5
+    item_counts["Y"] = 2
+    discount = apply_combibuy(item_counts)
     assert discount == 90
 
-    assert counts == {
-        "Z": 0,
-        "Y": 1
-    }
+    assert item_counts["Z"] == 0
+    assert item_counts["Y"] == 1
+
 
