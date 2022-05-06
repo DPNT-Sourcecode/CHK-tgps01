83cb11b302541591702f8ec1d40d179344ddbffd
sbx_user1051 <sbx_user1051@169.254.13.165>
2022-05-06 20:17:20 +0000

    Fri May 06 20:17:20 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,6 +1,14 @@
 
-
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
-    raise NotImplementedError()
+    try:
+        return checkout_implementation(skus)
+    except Exception:
+        return -1
+
+
+def checkout_implementation(input):
+    """Implementation of the function which raises exceptions instead of returning -1"""
+    pass
+
