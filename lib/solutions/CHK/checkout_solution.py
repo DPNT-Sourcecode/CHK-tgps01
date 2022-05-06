bda58042181b3b8cbbba10ec16b2eb0e71b58a3d
sbx_user1051 <sbx_user1051@169.254.13.165>
2022-05-06 21:49:35 +0000

    Fri May 06 21:49:35 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,6 +1,6 @@
-from typing import Dict, List
+from typing import Dict
 
-from .pricelist import load_prices
+from .pricelist import load_prices, PriceList
 
 
 # noinspection PyUnusedLocal
@@ -13,7 +13,7 @@ def checkout(skus):
         return -1
 
 
-def apply_deals(item_counts: Dict[str, int]) -> int:
+def apply_deals(pricelist: PriceList, item_counts: Dict[str, int]) -> int:
     """ Mutates the dictionary to apply the deals, and returns the discount
     obtained from the removed items"""
 
@@ -50,16 +50,17 @@ def calculate_sum(prices: Dict[str, int], item_counts: Dict[str, int]) -> int:
 def checkout_impl(letters):
     """Implementation of the function which raises exceptions instead of returning -1"""
 
-    prices = load_prices()
+    price_list = load_prices()
 
-    shopping_list_count = {key: 0 for key in prices}
+    shopping_list_count = {key: 0 for key in price_list}
 
     for letter in letters:
         shopping_list_count[letter] += 1
 
-    deal_total = apply_deals(shopping_list_count)
-    remaining_total = calculate_sum(prices, shopping_list_count)
+    deal_total = apply_deals(price_list, shopping_list_count)
+    remaining_total = calculate_sum(price_list.prices, shopping_list_count)
     return deal_total + remaining_total
+
 
 
 
