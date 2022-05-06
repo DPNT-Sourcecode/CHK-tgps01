0db4197f2d9d8d63b56dba6212f52a9976c85379
sbx_user1051 <sbx_user1051@169.254.13.165>
2022-05-06 22:11:30 +0000

    Fri May 06 22:11:30 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -20,19 +20,8 @@ def apply_deals(pricelist: PriceList, item_counts: Dict[str, int]) -> int:
     total_discount = 0
 
     for fb in pricelist.freebies:
-        n_present = item_counts[fb.item]
-        n_required = fb.n_required
-
-        if fb.item == "F":
-            print("...F...")
-            print(item_counts[fb.free_item])
-            print(n_present)
-            print(n_required)
-        item_counts[fb.free_item] -= (n_present // n_required)
+        item_counts[fb.free_item] -= (item_counts[fb.item] // (fb.n_required + 1))
         item_counts[fb.free_item] = max(0, item_counts[fb.free_item])
-        if fb.item == "F":
-            print(item_counts[fb.free_item])
-            print('..')
 
     for mb in pricelist.multibuys:
         total_discount += (item_counts[mb.item] // mb.n_required) * mb.cost
@@ -64,6 +53,7 @@ def checkout_impl(letters):
     deal_total = apply_deals(price_list, shopping_list_count)
     remaining_total = calculate_sum(price_list.prices, shopping_list_count)
     return deal_total + remaining_total
+
 
 
 
